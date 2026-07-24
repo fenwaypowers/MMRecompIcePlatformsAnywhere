6c116bda5e5e3deb9446d393aed99161205f2c0d
Fenway Powers <fppowers@protonmail.com>
2026-07-24 16:54:18 -0400

    Update queue_icefloe.c

--- src/queue_icefloe.c
+++ src/queue_icefloe.c
@@ -5,22 +5,34 @@
 #include "attributes.h"
 #include "overlays/actors/ovl_En_Arrow/z_en_arrow.h"
 
-RECOMP_HOOK("func_8088AA98") void before_func_8088AA98(EnArrow* this, PlayState* play) {
-    u32 enum_option = recomp_get_config_u32("enum_option");
-    recomp_printf("enum_option: %d\n", enum_option);
-
+void queue_icefloe(PlayState* play) {
     ObjectContext* objectCtx = &play->objectCtx;
 
     if (Object_GetSlot(objectCtx, OBJECT_ICEFLOE) == OBJECT_SLOT_NONE) {
 
+        if (objectCtx->numEntries >= ARRAY_COUNT(objectCtx->slots)) {
+            recomp_printf("No free object slots for OBJECT_ICEFLOE\n");
+            return;
+        }
+
         s32 slot = objectCtx->numEntries;
 
-        func_8012F73C(objectCtx, slot, OBJECT_ICEFLOE);
+        void* nextPtr = func_8012F73C(objectCtx, slot, OBJECT_ICEFLOE);
+
+        if (slot < ARRAY_COUNT(objectCtx->slots) - 1) {
+            objectCtx->slots[slot + 1].segment = nextPtr;
+        }
 
         objectCtx->numEntries++;
 
         recomp_printf("Queued OBJECT_ICEFLOE in slot %d\n", slot);
 
         return;
+    }
+}
+
+RECOMP_HOOK("func_8088AA98") void before_func_8088AA98(EnArrow* this, PlayState* play) {
+    if (recomp_get_config_u32("allow_anywhere") == 0) {
+        queue_icefloe(play);
     }
 }
