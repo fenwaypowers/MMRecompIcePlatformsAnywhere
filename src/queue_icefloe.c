5aeecb0ae7de3dd7810c57333052cbc8db32a411
Fenway Powers <fppowers@protonmail.com>
2026-07-24 17:10:14 -0400

    Update queue_icefloe.c

--- src/queue_icefloe.c
+++ src/queue_icefloe.c
@@ -9,25 +9,26 @@
 void queue_icefloe(PlayState* play) {
     ObjectContext* objectCtx = &play->objectCtx;
 
-    if (Object_GetSlot(objectCtx, OBJECT_ICEFLOE) == OBJECT_SLOT_NONE) {
-
-        if (objectCtx->numEntries >= ARRAY_COUNT(objectCtx->slots)) {
-            recomp_printf("No free object slots for OBJECT_ICEFLOE\n");
-            return;
-        }
-
-        s32 slot = objectCtx->numEntries;
-
-        void* nextPtr = func_8012F73C(objectCtx, slot, OBJECT_ICEFLOE);
-
-        if (slot < ARRAY_COUNT(objectCtx->slots) - 1) {
-            objectCtx->slots[slot + 1].segment = nextPtr;
-        }
-
-        objectCtx->numEntries++;
-
-        recomp_printf("Queued OBJECT_ICEFLOE in slot %d\n", slot);
+    if (Object_GetSlot(objectCtx, OBJECT_ICEFLOE) != OBJECT_SLOT_NONE) {
+        return;
     }
+
+    if (objectCtx->numEntries >= ARRAY_COUNT(objectCtx->slots)) {
+        recomp_printf("No free object slots for OBJECT_ICEFLOE\n");
+        return;
+    }
+
+    s32 slot = objectCtx->numEntries;
+
+    void* nextPtr = func_8012F73C(objectCtx, slot, OBJECT_ICEFLOE);
+
+    if (slot < ARRAY_COUNT(objectCtx->slots) - 1) {
+        objectCtx->slots[slot + 1].segment = nextPtr;
+    }
+
+    objectCtx->numEntries++;
+
+    recomp_printf("Queued OBJECT_ICEFLOE in slot %d\n", slot);
 }
 
 
